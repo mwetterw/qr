cbffbaddb9c2fb266290e54b28464cabedc6e562
Martin Wetterwald <martin@wetterwald.eu>
2021-11-22 01:28:39 +0100

    Handle odd chars & overflow for alphanumeric mode

--- src/qr/decoder.py
+++ src/qr/decoder.py
@@ -313,15 +313,27 @@ class QrCodeDecoder:
 
     @staticmethod
     def _decode_alphanumeric_segment(bitstream, char_count):
+        # Safety check for crazy char_count (overflow)
+        remaining_bits = len(bitstream.getvalue()) - bitstream.tell()
+        needed_bits = 11 * (char_count >> 1) + 6 * (char_count & 1)
+        if needed_bits > remaining_bits:
+            raise ValueError("Character count indicator overflow for alphanumeric segment")
+
         seg_data_buf = StringIO()
-        # FIXME: Add protection for crazy char_count
-        # TODO: Handle odd number of chars
-        for _ in range(0, char_count, 2):
+
+        # Handle an even number of characters
+        for _ in range(0, char_count & ~1, 2):
             double_char = int(bitstream.read(11), 2)
             char1 = double_char // 45
             char2 = double_char % 45
             seg_data_buf.write(consts.BASE45[char1])
             seg_data_buf.write(consts.BASE45[char2])
+
+        # Handle last character if the number of characters is odd
+        if char_count & 1:
+            char = int(bitstream.read(6), 2)
+            seg_data_buf.write(consts.BASE45[char])
+
         seg_data = seg_data_buf.getvalue()
         seg_data_buf.close()
         print(f"    {seg_data}")
--- tests/test_qr.py
+++ tests/test_qr.py
@@ -1,100 +1,111 @@
-import pytest
 from os.path import exists
 from os import makedirs
+from io import StringIO
 
-import qrcode
+import pytest
+
+from qrcode import QRCode
 from qrcode.constants import ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_Q, ERROR_CORRECT_H
 
 from qr.decoder import QrCodeDecoder
 
-EC_STR = ['L', 'M', 'Q', 'H']
-EC_DICT = {val: idx for idx, val in enumerate(EC_STR)}
-EC_QR = [ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_Q, ERROR_CORRECT_H]
 
-CAPACITY = [
-        17, 14, 11, 7,
-        32, 26, 20, 14,
-        53, 42, 32, 24,
-        78, 62, 46, 34,
-        106, 84, 60, 44,
-        134, 106, 74, 58,
-        154, 122, 86, 64,
-        192, 152, 108, 84,
-        230, 180, 130, 98,
-        271, 213, 151, 119,
-        321, 251, 177, 137,
-        367, 287, 203, 155,
-        425, 331, 241, 177,
-        458, 362, 258, 194,
-        520, 412, 292, 220,
-        586, 450, 322, 250,
-        644, 504, 364, 280,
-        718, 560, 394, 310,
-        792, 624, 442, 338,
-        858, 666, 482, 382,
-        929, 711, 509, 403,
-        1003, 779, 565, 439,
-        1091, 857, 611, 461,
-        1171, 911, 661, 511,
-        1273, 997, 715, 535,
-        1367, 1059, 751, 593,
-        1465, 1125, 805, 625,
-        1528, 1190, 868, 658,
-        1628, 1264, 908, 698,
-        1732, 1370, 982, 742,
-        1840, 1452, 1030, 790,
-        1952, 1538, 1112, 842,
-        2068, 1628, 1168, 898,
-        2188, 1722, 1228, 958,
-        2303, 1809, 1283, 983,
-        2431, 1911, 1351, 1051,
-        2563, 1989, 1423, 1093,
-        2699, 2099, 1499, 1139,
-        2809, 2213, 1579, 1219,
-        2953, 2331, 1663, 1273,
-]
+class TestDecodeAlphaNumericSegment:
+    @pytest.mark.parametrize(("expected", "bitstring"),
+        [   ("BCD+78", "001111110110100111000100101000011"),
+            ("AC-42", "0011100111011100111001000010"),
+            ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:", "000000000010000101110100010111001" \
+                "00100010101001011100010011100110101000101001010100001010101110000101100111101" \
+                "01110011001011111101011000101000110010101101101000010011010110010110111000001" \
+                "1100001110111001111001110110101011110011000111110001101101100"),
+            ("", ""),
+            ("/", "101011"),
+            ("O1", "1000011100100111000110")],
+        ids = ["simple_even", "simple_odd", "complete_charset", "zero_char_empty", "one_char",
+            "bitstream_underflow"])
+    def test_alphanumeric_valid(self, expected, bitstring, char_count=None):
+        if char_count is None:
+            char_count = len(expected)
+        bitstream = StringIO(bitstring)
+        try:
+            assert QrCodeDecoder._decode_alphanumeric_segment(bitstream, char_count) == expected
+        except:
+            bitstream.close()
+            raise
 
-LOREM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum volutpat porta. Sed nulla nibh, dapibus et egestas et, sodales nec felis. Aliquam erat volutpat. In in eleifend enim. Nunc aliquam ipsum eget libero sagittis, sed vestibulum justo ultricies. Praesent ornare nisl ut est facilisis, nec malesuada quam venenatis. Quisque sed aliquam erat. Etiam semper magna a dui consectetur, vel pretium ante fermentum. Vestibulum quis consectetur turpis, a viverra risus. Mauris sodales metus ac arcu fermentum, eu rhoncus sapien cursus. Nullam ac nunc sed elit sodales sollicitudin nec id libero.
-
-Etiam posuere, ipsum vel laoreet varius, purus enim sagittis velit, sit amet ultricies est ante ac diam. Sed commodo neque vel luctus laoreet. Aliquam erat volutpat. Ut ullamcorper, magna feugiat vehicula congue, nunc elit fringilla massa, quis lobortis purus leo at quam. Curabitur ornare pulvinar lacus in imperdiet. Mauris fermentum nunc vel libero feugiat, et suscipit quam aliquet. Nunc aliquet est condimentum, convallis ligula id, volutpat diam. Vestibulum sodales enim sit amet tincidunt suscipit. In dapibus, ligula a vehicula rhoncus, lorem tellus dictum dui, sit amet lacinia sem nunc et odio. Duis non tortor quam. Duis et mauris nec tellus blandit accumsan. Ut euismod vitae nisi vel molestie.
-
-Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis vel tincidunt sem. Pellentesque eu lacus vel ligula venenatis dignissim. Curabitur non suscipit massa. Donec blandit nisi et cursus imperdiet. Phasellus lacinia tincidunt feugiat. Cras a est quam. Donec ut mattis tortor. Aenean vitae tellus libero. Morbi nec sagittis nibh. Fusce sed sagittis nunc. Nulla eget vestibulum neque. Etiam enim nunc, egestas at cursus sit amet, commodo quis nulla. Vestibulum quis condimentum justo, id eleifend nulla. Donec ut aliquet sapien, quis lobortis quam.
-
-Suspendisse gravida, metus eget dapibus placerat, dui quam interdum tortor, eu facilisis massa est ac augue. In egestas velit metus, nec pharetra elit tristique sit amet. Morbi eros nisi, accumsan eu cursus eu, malesuada at erat. Curabitur convallis eu turpis non sollicitudin. Vivamus non erat hendrerit, fringilla enim ac, molestie ligula. Nam malesuada vulputate eros, vitae sollicitudin metus. Maecenas at lobortis purus, pharetra viverra nulla. Nulla sed finibus metus, nec dictum est. Praesent ornare mauris at magna pulvinar dapibus.
-
-Ut in arcu viverra, placerat mauris eget, dictum purus. Nunc eleifend tempor turpis ac imperdiet. Nam placerat tempus dolor sit amet mollis. Ut eget massa elit. Donec id gravida arcu. Sed a arcu sit amet purus lacinia suscipit. Aliquam imperdiet nunc quis finibus consequat. Nullam et euismod nibh. Donec scelerisque massa nec dolor gravida, ac suscipit sem aliquet. Vivamus convallis est a sapien fringilla tristique sed."""
+    @pytest.mark.parametrize(("expected", "bitstring", "char_count"),
+        [   ("O1",  "1000011101", None),
+            ("O14", "1000011100100100", None),
+            ("A1", "00111000011", 3),
+            ("O14", "10000111001000100", 4),
+            ("A1", "00111000011", 1031),
+            ("O14", "10000111001000100", 1031)],
+        ids = ["bitstream_even", "bitstream_odd", "bitstream_overflow_even",
+            "bitstream_overflow_odd", "bitstream_overflow_crazy_even",
+            "bitstream_overflow_crazy_odd"])
+    def test_alphanumeric_invalid(self, expected, bitstring, char_count):
+        with pytest.raises(ValueError):
+            self.test_alphanumeric_valid(expected, bitstring, char_count)
 
 
 class TestDecode:
+    EC_STR = ['L', 'M', 'Q', 'H']
+    EC_DICT = {val: idx for idx, val in enumerate(EC_STR)}
+    EC_QR = [ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_Q, ERROR_CORRECT_H]
+
+
     @pytest.fixture(scope="class", autouse=True)
     def create_qr_cache_dir(self):
         makedirs("tests/qr_cache", exist_ok=True)
 
+    @pytest.fixture(scope="class")
+    def lorem(self):
+        with open('tests/lorem.txt', 'r', encoding='utf-8') as file:
+            lorem = file.read()
+        return lorem
+
+    @pytest.fixture(scope="class")
+    def eightbit_capa(self):
+        return [
+            17, 14, 11, 7, 32, 26, 20, 14, 53, 42, 32, 24, 78, 62, 46, 34, 106, 84, 60, 44,
+            134, 106, 74, 58, 154, 122, 86, 64, 192, 152, 108, 84, 230, 180, 130, 98,
+            271, 213, 151, 119, 321, 251, 177, 137, 367, 287, 203, 155, 425, 331, 241, 177,
+            458, 362, 258, 194, 520, 412, 292, 220, 586, 450, 322, 250, 644, 504, 364, 280,
+            718, 560, 394, 310, 792, 624, 442, 338, 858, 666, 482, 382, 929, 711, 509, 403,
+            1003, 779, 565, 439, 1091, 857, 611, 461, 1171, 911, 661, 511, 1273, 997, 715, 535,
+            1367, 1059, 751, 593, 1465, 1125, 805, 625, 1528, 1190, 868, 658, 1628, 1264, 908, 698,
+            1732, 1370, 982, 742, 1840, 1452, 1030, 790, 1952, 1538, 1112, 842,
+            2068, 1628, 1168, 898, 2188, 1722, 1228, 958, 2303, 1809, 1283, 983,
+            2431, 1911, 1351, 1051, 2563, 1989, 1423, 1093, 2699, 2099, 1499, 1139,
+            2809, 2213, 1579, 1219, 2953, 2331, 1663, 1273
+        ]
+
     @pytest.mark.parametrize("ec_str", EC_STR)
-    @pytest.mark.parametrize("version", [v for v in range(1, 41)])
-    def test_decode_all_versions_with_8bit_max_size(self, version, ec_str):
-        ec = EC_DICT[ec_str]
-        capacity_idx = 4 * (version - 1) + ec
-        data = LOREM[:CAPACITY[capacity_idx]]
+    @pytest.mark.parametrize("version", list(range(1, 41)))
+    def test_decode_all_versions_with_8bit_max_size(self, lorem, eightbit_capa, version, ec_str):
+        ecl = self.EC_DICT[ec_str]
+        capacity_idx = 4 * (version - 1) + ecl
+        data = lorem[:eightbit_capa[capacity_idx]]
 
         filename = f"tests/qr_cache/{version}{ec_str}.qr"
 
         # If QR Code doesn't exist, generate it with a reference lib and cache
         # to disk to speed-up subsequent pytest runs
         if not exists(filename):
-            mask_pattern = (ec + version) % 8 # Prevent helper lib from choosing the best mask for each QR Code
-            qr = qrcode.QRCode(version=version, error_correction=EC_QR[ec], mask_pattern=mask_pattern)
-            qr.add_data(data)
-            qr.make(fit=False)
+            # Prevent helper lib from choosing the best mask for each QR Code
+            mask = (ecl + version) % 8
+            qr_code = QRCode(version=version, error_correction=self.EC_QR[ecl], mask_pattern=mask)
+            qr_code.add_data(data)
+            qr_code.make(fit=False)
             # Make sure the helper lib generated a QR code with the characteristics we want
-            assert qr.version == version
-            assert qr.error_correction == EC_QR[ec]
+            assert qr_code.version == version
+            assert qr_code.error_correction == self.EC_QR[ecl]
 
             # Cache generated QR Code to disk
-            with open(filename, "w") as f:
-                for row in qr.modules:
-                    f.write(''.join(str(int(module)) for module in row))
-                    f.write('\n')
+            with open(filename, "w", encoding="ascii") as file:
+                for row in qr_code.modules:
+                    file.write(''.join(str(int(module)) for module in row))
+                    file.write('\n')
 
         # Load reference QR Code from disk and check if we can retrieve the correct data
         my_qr = QrCodeDecoder(filename)
